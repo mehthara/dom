212bf4598d82488515ea6cfd65299cdfad89cf3a
Laize Ferraz <laizeferraz@gmail.com>
2021-01-07 08:01:49 +1100

    Files created.

---
+++ index.js
@@ -0,0 +1,17 @@
+//Your code goes here
+
+// 1. Click the button to display your name
+
+//2. Switch lights on mouseover
+
+//3. Double click to display content
+
+// 4. Traffic Lights
+
+//5. Change textbox border colours
+
+//6. validate the box [length should be more than 8 characters]
+
+// 7.create a list of hobbies
+
+// 8. Display a profile card from an object
